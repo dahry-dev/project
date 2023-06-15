852bc748ed3e0906f006a0491a0825d9a5d2b210
Dahrey <dahrianshor@gmail.com>
2023-06-15 20:57:23 +0700

    deleted

--- Type.php
+++
@@ -1,5 +0,0 @@
-                          <form id="form-delete-" action="{{ url('types/'.$type->id) }}" method="POST">
-                            @csrf @method('delete')
-                            <a href="#" class="btn btn-warning btn-sm" onClick="edit('{{ $type->id }}')"  data-toggle="modal" data-target="#updateSat">Edit</a>
-                            <button class="btn btn-sm btn-danger" type="submit">Delete</button>            
-                          </form>
